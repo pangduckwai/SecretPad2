9637a273659d5816871493bf90255e33b3554b12
pangduckwai <paul.lai@sea9.org>
2018-09-20 21:39:24 +0800

    Change text+button to SearchView on ActionBar

--- app/src/main/java/org/sea9/android/secret/ListActivity.kt
+++ app/src/main/java/org/sea9/android/secret/ListActivity.kt
@@ -1,10 +1,15 @@
 package org.sea9.android.secret
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
 
 import kotlinx.android.synthetic.main.activity_list.*
 
@@ -19,11 +24,27 @@ class ListActivity : AppCompatActivity() {
 			Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 					.setAction("Action", null).show()
 		}
+
+		// TODO Verify the action and get the query
+		if (Intent.ACTION_SEARCH == intent.action) {
+			intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+				Log.i("SecretPad2", query) // doSearchQuery(query)
+			}
+		}
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		menuInflater.inflate(R.menu.menu_list, menu)
+
+		// Get the SearchView and set the searchable configuration
+		val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+		(menu.findItem(R.id.menu_search).actionView as SearchView).apply {
+			// Assumes current activity is the searchable activity
+			setSearchableInfo(searchManager.getSearchableInfo(componentName))
+			setIconifiedByDefault(false) // Do not iconify the widget; expand it by default
+		}
+
 		return true
 	}
 
