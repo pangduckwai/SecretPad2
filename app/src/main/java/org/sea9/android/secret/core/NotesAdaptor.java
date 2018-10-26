c24b39e7d86887d901c7ba2ebcf9e3d2497bcd31
pangduckwai <paul.lai@sea9.org>
2018-10-27 00:39:18 +0800

    Clean-up warnings

--- app/src/main/java/org/sea9/android/secret/core/NotesAdaptor.java
+++ app/src/main/java/org/sea9/android/secret/core/NotesAdaptor.java
@@ -2,7 +2,6 @@ package org.sea9.android.secret.core;
 
 import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
