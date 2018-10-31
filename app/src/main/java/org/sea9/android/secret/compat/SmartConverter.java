8f22c51004b2a71f98409ba9c0ceaceef03c2144
pangduckwai <paul.lai@sea9.org>
2018-10-31 10:59:29 +0800

    Test smart converter

--- app/src/main/java/org/sea9/android/secret/compat/SmartConverter.java
+++ app/src/main/java/org/sea9/android/secret/compat/SmartConverter.java
@@ -1,15 +1,16 @@
 package org.sea9.android.secret.compat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SmartConverter {
 	private List<String> topics;
 
 	private SmartConverter(List<String> records) {
-		topics = new ArrayList<>();
-		for (String record : records)
-			topics.add(record.toLowerCase());
+		topics = records;
 	}
 
 	public static SmartConverter getInstance(List<String> records) {
@@ -17,6 +18,8 @@ public class SmartConverter {
 	}
 
 	private static final String NEWLINES = "\n\\s*\n";
+	private static final String NEWLINE = "\n";
+	private static final String SPACE = " ";
 	/**
 	 * <code>
 	 * Index:      0       1       2         3         4         5         6   ,...
@@ -25,24 +28,44 @@ public class SmartConverter {
 	 * Default   : old[1], old[3], old[1]  , old[4]  , old[5]  , old[2]
 	 * </code>
 	 */
-	public final String[][] convert(String category, String title, String content) {
-//		List<String[]> result = new ArrayList<>();
+	public final Map<String, List<String>> convert(String category, String title, String content) {
+		Map<String, List<String>> result = new HashMap<>(); //format is {key: [content, tags...]}
 
 		String[] contents = content.split(NEWLINES);
-		for (String topic : topics) {
-			for (String group : contents) {
-				if (group.trim().toLowerCase().startsWith(topic)) {
-
+		StringBuilder ctnt = new StringBuilder();
+		for (String group : contents) {
+			boolean found = false;
+			String[] lines = group.split(NEWLINE);
+			for (String topic : topics) {
+				if (lines[0].trim().toLowerCase().contains(topic.toLowerCase())) {
+					found = true;
+					String key = title + SPACE + lines[0];
+					StringBuilder ctn = new StringBuilder();
+					if (result.containsKey(key)) {
+						result.get(key).add(topic);
+					} else {
+						if (lines.length == 1) {
+							ctn.append(group);
+						} else { // lines.length > 1
+							for (int i = 1; i < lines.length; i++) {
+								if (i > 1) ctn.append(NEWLINE);
+								ctn.append(lines[i]);
+							}
+						}
+						List<String> values = new ArrayList<>();
+						values.add(ctn.toString());
+						values.add(topic);
+						values.add(category);
+						result.put(key, values);
+					}
 				}
 			}
+			if (!found) {
+				if (ctnt.length() > 0) ctnt.append(NEWLINES);
+				ctnt.append(group);
+			}
 		}
-
-		System.out.println("Category: " + category);
-		System.out.println("Title   : " + title);
-		System.out.println("Content :");
-		for (String content1 : contents)
-			System.out.println(">>> " + content1);
-
-		return null;
+		result.put(title, Arrays.asList(ctnt.toString(), category));
+		return result;
 	}
 }
--- app/src/test/java/org/sea9/android/secret/SecretPad2UnitTest.kt
+++ app/src/test/java/org/sea9/android/secret/SecretPad2UnitTest.kt
@@ -4,7 +4,6 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.sea9.android.secret.compat.SmartConverter
-import org.sea9.android.secret.crypto.CryptoUtils
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -19,18 +18,42 @@ class SecretPad2UnitTest {
 
 	@Test
 	fun testConverter() {
-		val tags: List<String> = arrayListOf("AAA", "BBB", "CCC")
-		val converter: SmartConverter = SmartConverter.getInstance(tags)
+		val tags = arrayListOf("AaA", "BBb", "cCC")
+		val converter = SmartConverter.getInstance(tags)
 		val content =
-						"AAA\n" +
-						"How are you\n" +
-						"I'm fine thanks\n\n" +
-						"CCC\n" +
-						"Yo Bro!!!\n" +
-						"  \n" +
-						"DDD\n" +
-						"XXXX:YYYY"
+				"AAA\n" +
+				"How are you\n" +
+				"I'm fine thanks\n\n" +
+				"CCC\n" +
+				"Yo Bro!!!\n" +
+				"  \n" +
+				"DDD\n" +
+				"XXXX:YYYY\n\n" +
+				"AAABBB\n" +
+				"Walawalawal Yoyoyo\n" +
+				"Cool man!\n"
 		val result = converter.convert("CAT0", "TTL1", content)
-		assertNull(result)
+		// TEMP >>>>>>>>>>>>>>>
+		for (entry in result.entries) {
+			System.out.println("[KEY] ${entry.key}")
+			if (entry.value.size > 0) {
+				System.out.println("[Content]")
+				System.out.println(entry.value[0])
+				System.out.print("[TAG] ")
+				for (i in 1 until entry.value.size) {
+					if (i > 1) System.out.print(" / ")
+					System.out.print(entry.value[i])
+				}
+				System.out.println()
+			}
+			System.out.println()
+		}
+		// TEMP <<<<<<<<<<<<<<<
+		assertTrue((result.size == 4) &&
+				(result["TTL1"]?.size == 2) &&
+				(result["TTL1 AAA"]?.size == 3) &&
+				(result["TTL1 CCC"]?.size == 3) &&
+				(result["TTL1 AAABBB"]?.size == 4)
+		)
 	}
 }
